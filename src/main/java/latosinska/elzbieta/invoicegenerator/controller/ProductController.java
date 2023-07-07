eccf9cdf41abbdf98f2389e1f45b843eac580c17
ELato <ela.latosinska@gmail.com>
2023-07-07 17:54:38 +0200

    added get Product by ID

--- src/main/java/latosinska/elzbieta/invoicegenerator/controller/ProductController.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/controller/ProductController.java
@@ -1,16 +1,15 @@
 package latosinska.elzbieta.invoicegenerator.controller;
 
+import latosinska.elzbieta.invoicegenerator.model.Category;
 import latosinska.elzbieta.invoicegenerator.model.Product;
 import latosinska.elzbieta.invoicegenerator.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "localhost:8081")
 @RestController
@@ -29,4 +28,15 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isPresent()) {
+            return new ResponseEntity<>(product.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
