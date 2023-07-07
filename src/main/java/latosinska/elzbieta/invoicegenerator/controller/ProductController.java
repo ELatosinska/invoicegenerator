8c15cec7c1e0897f147215a32d9e02c1ab6ec96a
ELato <ela.latosinska@gmail.com>
2023-07-07 17:00:38 +0200

    added get all products endpoint

--- src/main/java/latosinska/elzbieta/invoicegenerator/controller/ProductController.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/controller/ProductController.java
@@ -1,17 +1,32 @@
 package latosinska.elzbieta.invoicegenerator.controller;
 
+import latosinska.elzbieta.invoicegenerator.model.Product;
 import latosinska.elzbieta.invoicegenerator.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @CrossOrigin(origins = "localhost:8081")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/products")
 public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        try {
+            List<Product> products = productRepository.findAll();
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } catch(Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
