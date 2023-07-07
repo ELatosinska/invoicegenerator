119552ef8939c4bada8df8f9099df8df48395802
ELato <ela.latosinska@gmail.com>
2023-07-07 16:47:53 +0200

    added product repository

---
+++ src/main/java/latosinska/elzbieta/invoicegenerator/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package latosinska.elzbieta.invoicegenerator.repository;
+
+import latosinska.elzbieta.invoicegenerator.model.Category;
+import latosinska.elzbieta.invoicegenerator.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByCategory(Category category);
+}
