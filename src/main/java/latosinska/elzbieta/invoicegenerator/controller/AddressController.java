735ec5cb7dfc74a65fe7203b83e0fe055c5dea5f
ELato <ela.latosinska@gmail.com>
2023-07-10 16:00:42 +0200

    created put method for address

--- src/main/java/latosinska/elzbieta/invoicegenerator/controller/AddressController.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/controller/AddressController.java
@@ -3,17 +3,19 @@ package latosinska.elzbieta.invoicegenerator.controller;
 import jakarta.annotation.Resource;
 import latosinska.elzbieta.invoicegenerator.model.Address;
 import latosinska.elzbieta.invoicegenerator.repository.AddressRepository;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "localhost:8081")
 @RestController
 @RequestMapping("/api/addresses")
-public class AddressController{
+public class AddressController {
     @Resource
     AddressRepository addressRepository;
 
@@ -34,9 +36,30 @@ public class AddressController{
     public ResponseEntity<Address> createAddress(@RequestBody Address address) {
         try {
             return new ResponseEntity<>(addressRepository.save(address), HttpStatus.CREATED);
-        } catch(Exception ex) {
-            System.out.println(ex.getMessage());
-            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddress(@RequestBody Address address, @PathVariable("id") Long id) {
+        try {
+            Optional<Address> addressToUpdate = addressRepository.findById(id);
+            if (addressToUpdate.isPresent()) {
+                Address updatingAddress = addressToUpdate.get();
+                if (address.getCity() != null) updatingAddress.setCity(address.getCity());
+                if (address.getCountry() != null) updatingAddress.setCountry(address.getCountry());
+                if (address.getPostalCode() != null) updatingAddress.setPostalCode(address.getPostalCode());
+                if (address.getApartmentNumber() != null)
+                    updatingAddress.setApartmentNumber(address.getApartmentNumber());
+                if (address.getBuildingNumber() != null) updatingAddress.setBuildingNumber(address.getBuildingNumber());
+                if (address.getStreet() != null) updatingAddress.setStreet(address.getStreet());
+                return new ResponseEntity<>(addressRepository.save(updatingAddress), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
