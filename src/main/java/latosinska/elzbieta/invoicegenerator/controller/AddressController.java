df3f1440c50ff7a0a29839800cf1862c4b0b0728
ELato <ela.latosinska@gmail.com>
2023-07-10 12:33:58 +0200

    added get mapping for all addresses

--- src/main/java/latosinska/elzbieta/invoicegenerator/controller/AddressController.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/controller/AddressController.java
@@ -1,15 +1,27 @@
 package latosinska.elzbieta.invoicegenerator.controller;
 
+import latosinska.elzbieta.invoicegenerator.model.Address;
+import latosinska.elzbieta.invoicegenerator.model.Category;
 import latosinska.elzbieta.invoicegenerator.repository.AddressRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins = "localhost:8081")
 @RestController
 @RequestMapping("/api/addresses")
 public class AddressController{
+    @Autowired
     AddressRepository addressRepository;
 
+    @GetMapping
+    public ResponseEntity<List<Address>> getAddresses() {
+        List<Address> addresses = new ArrayList<>(addressRepository.findAll());
+        return addresses.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
 
 }
