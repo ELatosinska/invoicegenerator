254d7b3408a52f2529a9e515fd303666a1ff9bb2
ELato <ela.latosinska@gmail.com>
2023-07-10 10:04:23 +0200

    created constructors for invoice

--- src/main/java/latosinska/elzbieta/invoicegenerator/model/Invoice.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/model/Invoice.java
@@ -2,12 +2,15 @@ package latosinska.elzbieta.invoicegenerator.model;
 
 import jakarta.persistence.*;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
 @Entity
 @Table(name = "invoices")
 public class Invoice {
+
+    private static int duePeriodInDays = 14;
     @Column(name = "created_date")
     private Date createdDate;
     @Column(name = "due_date")
@@ -21,4 +24,29 @@ public class Invoice {
     private Company vendee;
     @OneToMany(mappedBy = "invoice")
     private Collection<InvoiceItem> items;
+
+    {
+        createdDate = new Date(System.currentTimeMillis());
+        dueDate =setDueDate(createdDate);
+    }
+
+    public Invoice() {
+    }
+
+    public Invoice(Company vendor, Company vendee) {
+        this.vendor = vendor;
+        this.vendee = vendee;
+    }
+
+    public void setDuePeriodInDays(int daysToPayInvoice) {
+        if(daysToPayInvoice >= 0) duePeriodInDays = daysToPayInvoice;
+        // TODO: what to do when days less than 0
+    }
+
+    private Date setDueDate(Date createdDate) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(createdDate);
+        calendar.add(Calendar.DATE, duePeriodInDays);
+        return calendar.getTime();
+    }
 }
---
+++ src/main/java/latosinska/elzbieta/invoicegenerator/service/InvoiceService.java
@@ -0,0 +1,8 @@
+package latosinska.elzbieta.invoicegenerator.service;
+
+import latosinska.elzbieta.invoicegenerator.model.Invoice;
+
+public class InvoiceService {
+    private Invoice invoice;
+
+}
