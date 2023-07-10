96e4135c2e78f156cd8c910195f5601b688c0ed2
ELato <ela.latosinska@gmail.com>
2023-07-10 12:39:19 +0200

    added get mapping for address by id

--- src/main/java/latosinska/elzbieta/invoicegenerator/controller/AddressController.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/controller/AddressController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "localhost:8081")
 @RestController
@@ -22,6 +23,13 @@ public class AddressController{
     public ResponseEntity<List<Address>> getAddresses() {
         List<Address> addresses = new ArrayList<>(addressRepository.findAll());
         return addresses.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable("id") Long id) {
+        return addressRepository.findById(id)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 }
