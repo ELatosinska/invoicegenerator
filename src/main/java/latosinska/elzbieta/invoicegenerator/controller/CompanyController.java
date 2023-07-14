7369c8ece5f369771b757b00c9f7d994dccf266f
ELato <ela.latosinska@gmail.com>
2023-07-14 15:36:27 +0200

    created getCompanyById method

--- src/main/java/latosinska/elzbieta/invoicegenerator/controller/CompanyController.java
+++ src/main/java/latosinska/elzbieta/invoicegenerator/controller/CompanyController.java
@@ -5,12 +5,11 @@ import latosinska.elzbieta.invoicegenerator.model.Company;
 import latosinska.elzbieta.invoicegenerator.repository.CompanyRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @CrossOrigin(origins="localhost:8081")
 @RestController
 @RequestMapping(value="/api/companies")
@@ -21,5 +20,12 @@ public class CompanyController {
     @GetMapping
     public ResponseEntity<List<Company>> getAllCompanies() {
         return new ResponseEntity<>(companyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable("id") Long id) {
+        Optional<Company> company = companyRepository.findById(id);
+        if(company.isEmpty()) return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(company.get(), HttpStatus.OK);
     }
 }
